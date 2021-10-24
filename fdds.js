cac00ba12184105f0e23d5c0bbeb67d1a114c88d
Zhiiqiiang <58096829+Zhiiqiiang@users.noreply.github.com>
2021-10-24 19:38:49 +0800

    Update fdds.js

--- fdds.js
+++ fdds.js
@@ -1,6 +1,6 @@
 /*
 #樊登读书 unlock vip by 渤涵  buehen0426/Scripts/fdds/fdds.js
-fdds = type=http-response,pattern=^https:\/\/api\.dushu\.io\/(userInfo|books\/freeList|app\/hasUserBlackCard),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Zhiiqiiang/HZZQ/master/nfzm_user.js,script-update-interval=0
+fdds = type=http-response,pattern=^https:\/\/api\.dushu\.io\/(userInfo|books\/freeList|app\/hasUserBlackCard),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Zhiiqiiang/HZZQ/master/fdds.js,script-update-interval=0
 
 hostname = api.dushu.io
 */
